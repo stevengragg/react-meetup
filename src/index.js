079678747700800b1ccfbb351a561d113c024550
sgragg5619 <sgragg5619@gmail.com>
2021-12-12 00:42:25 +0800

    Setup on root files

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+// Index.js is the first code that will load on the browser
+
 ReactDOM.render(<App />, document.getElementById("root"));
