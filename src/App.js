044c45a31639f1adafde930fc8fd7fd4945034aa
sgragg5619 <sgragg5619@gmail.com>
2021-12-12 00:35:32 +0800

    Clean up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+/**
+ * @description App function is the root react component for this project
+ * @returns App
+ */
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world!</h1>
     </div>
   );
 }
